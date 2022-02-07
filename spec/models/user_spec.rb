00b9f419642913c7bde9d4c6b604073a01775b4f
SGrossett <selene_816@hotmail.com>
2022-02-07 18:11:33 -0500

    define validation specs for password, password_confirmation, and missing fields.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,12 +2,70 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   
-  describe 'Validations' do
-    # validation examples here
-  end
+  let(:user) { User.create(first_name: "Selene", last_name: "Grossett", email: "example@example.com", password: "password", password_confirmation: "password") }
 
-  describe '.authenticate_with_credentials' do
-    # examples for this class method here
-  end
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      expect(user).to be_valid
+    end
+
+    it 'is not valid if the password and password_confirmation do not match' do
+      @user = User.create(first_name: "Penny", last_name: "Proud", email: "example@example.com", password: "password", password_confirmation: "incorrect")
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it 'is not valid without a password' do
+      @user = User.create(first_name: "King", last_name: "Bob", email: "example@example.com", password_confirmation: "incorrect")
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'is not valid without a password confirmation' do
+      @user = User.create(first_name: "Ron", last_name: "Stopable", email: "example@example.com", password: "password")
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Password confirmation can't be blank")
+    end
+
+    it 'is not valid if the email is not unique' do
+      @user = User.create(first_name: "Selene", last_name: "Grossett", email: "example@example.com", password: "password", password_confirmation: "password")
+      @user2 = User.create(first_name: "Avatar", last_name: "Aang", email: "example@example.com", password: "password", password_confirmation: "password")
+      expect(@user2).to_not be_valid
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it 'is not valid if the email is not unique, regardless of case' do
+      @user = User.create(first_name: "Selene", last_name: "Grossett", email: "example@example.com", password: "password", password_confirmation: "password")
+      @user2 = User.create(first_name: "Kim", last_name: "Possible", email: "EXAMPLE@EXAMPLE.com", password: "password", password_confirmation: "password")
+      expect(@user2).to_not be_valid
+      expect(@user2.errors.full_messages).to include("Email has already been taken")
+    end
+
+     it 'is not valid without a email' do
+      @user = User.create(first_name: "Tino", last_name: "Tonitini", password: "password", password_confirmation: "password")
+      # user.email = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it 'is not valid without a first name' do
+      @user = User.create(last_name: "Descartes", password: "password", email: "example@example.com", password_confirmation: "password")
+
+      user.first_name = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it 'is not valid without a last name' do
+      @user = User.create(first_name: "Randal", password: "password", email: "example@example.com", password_confirmation: "password")
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
   
+
+    describe '.authenticate_with_credentials' do
+
+    end
+  end
 end
