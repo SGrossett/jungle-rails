7fe3850273c3335edba19ac3e50a4952d86501e8
SGrossett <selene_816@hotmail.com>
2022-02-07 19:02:37 -0500

    Create validations for product specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Product, type: :model do
     subject { described_class.new(name: "Awesome, Massive TV", price_cents: 10000, quantity: 21, category: category)}
 
     it 'is valid with valid attributes' do
-    expect(subject).to be_valid
+      expect(subject).to be_valid
     end
 
     it 'is not valid without a name' do
